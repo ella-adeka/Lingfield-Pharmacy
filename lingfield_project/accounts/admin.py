b8fd2c2c1f27ee41cecab1891bff13523db5e136
Emmanuella Adeka <onyiadeka@gmail.com>
2021-01-13 02:36:26 +0100

    added MedicineItems model

--- lingfield_project/accounts/admin.py
+++ lingfield_project/accounts/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from .models import User,Dependent,UserBirthDate, UserProfile, HospitalList, AddSurgery, PrescriptionItem, Prescription
+from .models import User,Dependent,UserBirthDate, UserProfile, HospitalList, AddSurgery,MedicineItems, PrescriptionItem, Prescription
 
 # Register your models here.
 
 admin.site.register(AddSurgery)
 admin.site.register(Dependent)
 admin.site.register(HospitalList)
+admin.site.register(MedicineItems)
 admin.site.register(PrescriptionItem)
 admin.site.register(Prescription)
 admin.site.register(UserBirthDate)
