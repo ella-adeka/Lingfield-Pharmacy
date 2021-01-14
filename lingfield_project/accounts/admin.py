113cf5fcdfc17331c1abbaa75bec72bdeb77970e
Emmanuella Adeka <onyiadeka@gmail.com>
2021-01-14 02:23:51 +0100

    added SelectSurgery model

--- lingfield_project/accounts/admin.py
+++ lingfield_project/accounts/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User,Dependent,UserBirthDate, UserProfile, HospitalList, AddSurgery,MedicineItems, PrescriptionItem, Prescription
+from .models import *
 
 # Register your models here.
 
@@ -9,6 +9,7 @@ admin.site.register(HospitalList)
 admin.site.register(MedicineItems)
 admin.site.register(PrescriptionItem)
 admin.site.register(Prescription)
+admin.site.register(SelectSurgery)
 admin.site.register(UserBirthDate)
 admin.site.register(UserProfile)
 
