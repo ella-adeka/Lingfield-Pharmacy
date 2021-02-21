6433cad347c4d2ebe00ef964fe5ee478307633a5
Emmanuella Adeka <onyiadeka@gmail.com>
2021-02-21 23:41:02 +0100

    Changed DB

--- lingfield_project/lingfield_project/settings.py
+++ lingfield_project/lingfield_project/settings.py
@@ -99,7 +99,7 @@ WSGI_APPLICATION = 'lingfield_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'lingfield2DB',
+        'NAME': 'lingfield3DB',
         'USER': 'postgres',
         'PASSWORD': 'november1899',
         'HOST':'localhost'
