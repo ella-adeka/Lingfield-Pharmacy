105c030dee285874fad8ce0729df78d63bc2f48a
Emmanuella Adeka <onyiadeka@gmail.com>
2021-02-25 23:32:43 +0100

    Added PrescriptionItemAdmin & PrescriptionAdmin

--- lingfield_project/accounts/admin.py
+++ lingfield_project/accounts/admin.py
@@ -2,13 +2,28 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
+class PrescriptionItemAdmin(admin.ModelAdmin):
+    list_display = ("user","get_medicine_items","selected_surgery")
+
+    def get_medicine_items(self, obj):
+        return "\n,".join([str(i)for i in obj.medicine_items.all()])
+
+class PrescriptionAdmin(admin.ModelAdmin):
+    list_display = ("user","get_prescription_items")
+
+    def get_prescription_items(self, obj):
+        return "\n".join([str(i)for i in obj.items.all()])
+
+
+
+# admin.site.register(Category,CategoryAdmin)
 
 admin.site.register(AddSurgery)
 admin.site.register(Dependent)
 admin.site.register(HospitalList)
 admin.site.register(MedicineItems)
-admin.site.register(Prescription)
-admin.site.register(PrescriptionItem)
+admin.site.register(Prescription, PrescriptionAdmin)
+admin.site.register(PrescriptionItem, PrescriptionItemAdmin)
 admin.site.register(SelectSurgery)
 admin.site.register(UserBirthDate)
 admin.site.register(UserProfile)
