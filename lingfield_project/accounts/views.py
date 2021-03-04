b71e8191ed6266bcbe5ea802ab366434f04d4a74
Emmanuella Adeka <onyiadeka@gmail.com>
2021-03-04 23:46:57 +0100

    Edited delete view

--- lingfield_project/accounts/views.py
+++ lingfield_project/accounts/views.py
@@ -137,6 +137,7 @@ class DashboardView(LoginRequiredMixin, View):
                 ) 
                 for item in ordered_item:
                     prescription.items.add(item)
+                    messages.info(request,  f'Order Added!')
 
                 prescription_item_qs = PrescriptionItem.objects.filter(
                     user=request.user,
@@ -271,21 +272,15 @@ def delete_prescription(request, id):
                     for item in med_item_qs:
                         pres_item.medicine_items.remove(item)
                         item.delete()
-                    pres_item.delete()
-                    pres.delete()
                     messages.info(request, "The specific order was deleted.")
                     return redirect("accounts:dashboard")
                 elif (pres_item.medicine_items.count() == 1):
                     pres_item.medicine_items.remove(item)
                     item.delete()
-                    pres_item.delete()
-                    pres.delete()
                     messages.info(request, "The medicine items were deleted.")
                     return redirect("accounts:dashboard")
                 else:
                     item.delete()
-                    pres_item.delete()
-                    pres.delete()
                     messages.info(request, "All orders were deleted.")
                     return redirect("accounts:dashboard")
             else:
@@ -451,9 +446,9 @@ def delete_prescription_item(request, id):
        
         # if(PrescriptionItem.objects.filter(user=request.user, pk=id)):
         if (pres_item.medicine_items.count() > 1):
-            medicine_item = MedicineItems.objects.filter(user=request.user, added=True)
-            for item in med_item_qs:
-                item.delete()
+            medicine_item = MedicineItems.objects.filter(user=request.user, added=True).delete()
+            # for item in medicine_item:
+            #     item.delete()
             messages.info(request, "Items were deleted.")
         else:
             item = MedicineItems.objects.get(user=request.user, added=True)
